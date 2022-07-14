42c81516a479a6cf6655e0d7967184841000f1e6
Sundaresan <rsundaresan007@gmail.com>
2022-07-14 14:39:15 +0530

    Audit-severity

---
+++ audit-severity/src/main/java/com/mfpe/repository/AuditResponseRepo.java
@@ -0,0 +1,10 @@
+package com.mfpe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mfpe.model.AuditResponse;
+
+
+public interface AuditResponseRepo extends JpaRepository<AuditResponse, Integer> {
+
+}

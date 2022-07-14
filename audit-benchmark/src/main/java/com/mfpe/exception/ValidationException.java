798b187139dbbf707483b84e3d15529a1d8e28c8
Sundaresan <rsundaresan007@gmail.com>
2022-07-14 14:33:27 +0530

    Audit-benchmark

---
+++ audit-benchmark/src/main/java/com/mfpe/exception/ValidationException.java
@@ -0,0 +1,15 @@
+package com.mfpe.exception;
+
+
+public class ValidationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException(String message) {
+		super(message);
+	}
+
+	public ValidationException() {
+
+	}
+
+}

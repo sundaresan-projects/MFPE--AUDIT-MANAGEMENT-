cdbe62bd5f703875fe3423a7879796b1ce4c23d5
Sundaresan <rsundaresan007@gmail.com>
2022-07-14 14:25:51 +0530

    Authorization

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Sundaresan', 'sun', 'sun000', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'Kaushal', 'kp', 'kp000', 'Project-2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'Aishwarya', 'ap', 'ap000', 'Project-3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'jyothirmai', 'jv', 'jv000', 'Project-4');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (5, 'Mani', 'mb', 'mb000', 'Project-5');
+
---
+++ audit-authorization/src/test/java/com/mfpe/AuditAuthorizationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mfpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class AuditAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
